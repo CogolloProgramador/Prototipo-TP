eb3d23a4d8f33e71cd17716115e5d58e1951c09b
Lautaro <lautarodamian20@gmail.com>
2022-10-18 16:23:12 -0300

    sigo intentando con el json

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,7 @@ let url="./javascript/data.json"
 let datos
 
 fetch(url)
-.then(response => { return response.json();})
+.then(response => response.json())
 .then(data => {console.log(data)
     datos = data 
    } 
