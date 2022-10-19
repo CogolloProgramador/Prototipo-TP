86147f452156cd8aeda5f5cc09311148db0aa212
Lautaro <lautarodamian20@gmail.com>
2022-10-18 21:54:55 -0300

    json activo, comienzo de cad en java

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,21 @@
-let url="./javascript/data.json"
+let url = "./javascript/data.json"
 let datos
 
 fetch(url)
-.then(response => response.json())
-.then(data => {console.log(data)
-    datos = data 
-   } 
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        datos = data
+    }
     );
-    
+
+    var cad= ``
+
+    for (element of data){
+       cad=cad + `<img src=${imagen} alt="book_cover"></img>
+       `
+    }
+
+
+    console.log (cad)
+    //document.getElementById("boxcontainer").innerHTML=cad
